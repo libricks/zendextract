28d0d92304132b44fc6834a165db44946f9b9e08
Bastien Bouvet <bvtbastien@gmail.com>
2018-05-23 13:40:42 +0200

    Changement chemin vendor dans ExtractionController (v => V)

--- lib/Controller/ExtractionController.php
+++ lib/Controller/ExtractionController.php
@@ -22,7 +22,7 @@ ini_set("display_errors", 1);
 /**
  *
  */
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../Vendor/autoload.php';
 
 //use Zendesk\API\HttpClient as ZendeskAPI;
 
