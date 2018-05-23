3ad3a936441d742cbdb22d96b0af0f38b2396864
Bastien Bouvet <bvtbastien@gmail.com>
2018-05-23 11:50:30 +0200

    Retour sur l'ancienne version de l'API

--- lib/Service/ZendDeskAPI.php
+++ lib/Service/ZendDeskAPI.php
@@ -10,77 +10,49 @@
  * @license AGPL
  * @license https://opensource.org/licenses/AGPL-3.0
  */
-
 namespace OCA\ZendExtract\Service;
-
 use \OCP\IConfig;
-
-require_once __DIR__ . '/../../vendor/autoload.php';
-use Zendesk\API\HttpClient as ZendeskAP;
 class ZendDeskAPI
 {
-
-
     private $subdomain;
     private $username;
     private $token;
-    private $client;
-
+    private $uri;
     public function __construct($AppName, IConfig $config)
     {
-
         $this->subdomain = $config->getAppValue($AppName, 'zendextract_domain');
         $this->username = $config->getAppValue($AppName, 'zendextract_email');
         $this->token = $config->getAppValue($AppName, 'zendextract_token');
         // TODO : Check how to put this URI in a variable.
-
-        $this->client = new ZendeskAP($this->subdomain);
-        $this->client->setAuth('basic', ['username' => $this->username, 'token' => $this->token]);
-
+        $this->uri = "https://$this->subdomain.zendesk.com";
     }
-
     public function get($endpoint, $params)
     {
         $response = "";
         try {
-
-          //  $response = \Httpful\Request::get($this->uri . $endpoint)
-          //      ->sendsJson()
-          //      ->authenticateWith($this->username . "/token", $this->token)
-          //      ->send();
-            //          $result = $response->body;
-
-
-
-      //      $result = $this->client->tickets()->findAll();
-
-
-            $result = $this->client->get($endpoint);
-
+            $response = \Httpful\Request::get($this->uri . $endpoint)
+                ->sendsJson()
+                ->authenticateWith($this->username . "/token", $this->token)
+                ->send();
+            $result = $response->body;
             return $result;
         } catch (Httpful\Exception $e) {
-                $this->logger->error("Problème lors de la récupération des tickets " . $e->getMessage(), array('app' => $this->appName));
-
+            $this->logger->error("Problème lors de la récupération des tickets " . $e->getMessage(), array('app' => $this->appName));
         } catch (Exception $e) {
             $this->logger->error("Problème API : " . $e->getMessage() . " ---------" . $response, array('app' => $this->appName));
         }
     }
-
     public function getAbsolute($uri)
     {
-
         try {
-
-           /* $response = \Httpful\Request::get($uri)
+            $response = \Httpful\Request::get($uri)
                 ->sendsJson()
                 ->authenticateWith($this->username . "/token", $this->token)
                 ->send();
-            $result = $response->body;*/
-            $result = $this->client->tickets()->find($uri);
+            $result = $response->body;
             return $result;
         } catch (Exception $e) {
             return null;
         }
     }
-
 }
