c42ae5785eb330a6c19fdc7070f234a313ffc23b
Tawfiq CADI TAZI <tawfiq@caditazi.fr>
2019-03-02 16:43:17 +0100

    Suppression du bouton "ouvrir le fichier" à la suite d'une génération

--- templates/content/generate.php
+++ templates/content/generate.php
@@ -3,4 +3,3 @@
 </p>
 
 <a href="<?php echo $_["webRoot"] ?>/index.php/apps/files/?dir=/Extractions/Findus&fileid=<?php echo $_["folder"]->getId()?>" class="btn btn-success">Voir le dossier de l'extraction</a>
-<a href="<?php echo $_["webRoot"] ?>/index.php/apps/files/?dir=/Extractions/Findus&fileid=<?php echo $_["file"]->getId()?>" class="btn btn-success">Voir le fichier de l'extraction</a>
