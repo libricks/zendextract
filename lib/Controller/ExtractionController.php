43ed3b72a9282af52d3768f2decfd9179d2fe808
Tawfiq <tawfiq@caditazi.fr>
2024-01-30 16:49:59 +0100

    Update defaultpath parameter in step1POST method

--- lib/Controller/ExtractionController.php
+++ lib/Controller/ExtractionController.php
@@ -366,7 +366,7 @@ class ExtractionController extends Controller
      * @param   integer $id |null
      * @return  RedirectResponse
      */
-    public function step1POST($name, $forms, $defaultpath = "", $mode, $brand_id, $newbrand, $id = null,$group)
+    public function step1POST($name, $forms, $defaultpath = "test", $mode, $brand_id, $newbrand, $id = null,$group)
     {
 
         //Création de l'extraction
