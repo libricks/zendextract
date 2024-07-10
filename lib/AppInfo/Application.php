47b55e3d6031c141262cca6bc98f97e3ff0ef77f
tawfiq <tawfiq@caditazi.fr>
2024-07-10 11:38:28 +0200

    Update

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -18,6 +18,7 @@ class Application extends App implements IBootstrap {
 	}
 
 	public function register(IRegistrationContext $context): void {
+        require_once  __DIR__ . '/../../vendor/autoload.php';
 	}
 
 	public function boot(IBootContext $context): void {
